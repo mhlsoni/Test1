7c03ee4286abc4e3970a6d1b01971ba1cfbf5098
System Administrator <root@vin0l209291.man.co>
2017-12-05 20:32:48 -0600

    Test Java Jenkins

---
+++ TestFile.java
@@ -0,0 +1,11 @@
+/**
+ * Created by mbhadiyadr on 12/5/17.
+ */
+public class TestFile {
+
+    public static void main(String[] args) {
+        for(int i=0; i<10; i++) {
+            System.out.println("testing...."+i);
+        }
+    }
+}
